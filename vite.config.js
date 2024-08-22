7147364154ee7b66ddf78603f1e15cd3d221c85b
Dinusha-ChathurangaCSE <dinusha.21@cse.mrt.ac.lk>
2024-08-22 16:27:23 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"/3D PORTFOLIO/project_3D_developer_portfolio/",
+  plugins: [react()],
+})
